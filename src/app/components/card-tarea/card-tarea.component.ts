d381199b868d4bacf4db78fb64ded9ba390bc778
Mateo Ramirez Uribe <mateo.ramirez.uribe@gmail.com>
2025-03-17 07:39:50 -0500

    se realizan ajustes en los estilos de card tarea y se establece una nueva función para establecer el ícono de tipo de tarea

--- src/app/components/card-tarea/card-tarea.component.ts
+++ src/app/components/card-tarea/card-tarea.component.ts
@@ -22,4 +22,48 @@ export class CardTareaComponent {
     return this.funcionesGlobales.establecerColorPrioridad(priorodad);
   }
 
+  establecerIconoTipo(tipo: 'laboral' | 'academica' | 'familiar' | 'hogar' | 'entretenimiento' | 'recreacion' | 'viaje') {
+    switch (tipo) {
+      case 'laboral':
+        return 'bi bi-hammer'
+        break;
+      case 'academica':
+        return 'bi bi-laptop'
+        break;
+      case 'familiar':
+        return 'bi bi-person-heart'
+        break;
+      case 'hogar':
+        return 'bi bi-house-heart-fill'
+        break;
+      case 'entretenimiento':
+        return 'bi bi bi-controller'
+        break;
+      case 'recreacion':
+        return 'bi bi-balloon-fill'
+        break;
+      case 'viaje':
+        return 'bi bi-luggage-fill'
+        break;
+      default:
+        return 'bi bi-laptop';
+        break;
+    }
+  }
+
+  /*                     @if (tarea.tipo === 'laboral') {
+                        <p><em class="bi bi-hammer "></em></p>
+                    } @else if (tarea.tipo === 'academica') {
+                        <p><em class="bi bi-laptop"></em></p>
+                    } @else if (tarea.tipo === 'familiar') {
+                        <p><em class="bi bi-person-heart"></em></p>
+                    } @else if (tarea.tipo === 'hogar') {
+                        <p><em class="bi bi-house-heart-fill"></em></p>
+                    } @else if (tarea.tipo === 'entretenimiento') {
+                        <p><em class="bi bi-controller"></em></p>
+                    } @else if (tarea.tipo === 'recreacion') {
+                        <p><em class="bi bi-balloon-fill"></em></p>
+                    } @else if (tarea.tipo === 'viaje') {
+                        <p><em class="bi bi-luggage-fill"></em></p>
+                    } */
 }
